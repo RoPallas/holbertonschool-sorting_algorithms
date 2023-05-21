b8b6c26ad6fab8a50b7c429eb25a83c6e1dadfd9
Mariana Echeto <echetomariana@gmail.com>
2023-05-20 23:49:42 -0300

    added counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+/**
+ * counting_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of array
+*/
+void counting_sort(int *array, size_t size)
+{
+	int max = array[0];
+	size_t i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	int *output = (int *)malloc(size * sizeof(int));
+
+	if (output == NULL)
+		return;
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	int *count = (int *)malloc((max + 1) * sizeof(int));
+
+	if (count == NULL)
+	{
+		free(output);
+		return;
+	}
+
+	for (i = 0; (int)i <= (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	for (i = 1; (int)i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < size ; i++)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+
+	free(count);
+	free(output);
+}
